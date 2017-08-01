559f92073ac2137fc5c1169a85629aa4752a7d3c
DeadManWalker <kev.hambrecht@gmail.com>
2017-08-01 20:34:20 +0200

    fixed .gitignore, fixed project name

--- build/server.js
+++ build/server.js
@@ -11,32 +11,23 @@ var PUBLIC_DIR = path.join(__dirname, "../public");
 var CSS_DIR = path.join(PUBLIC_DIR, "/css");
 var LESS_DIR = path.join(PUBLIC_DIR, "/less");
 var app = express();
-// Template/ View Engine
 app.set("views", VIEWS_DIR);
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
-// Less
 app.use(lessMiddleware(LESS_DIR, {
     dest: CSS_DIR,
     compress: true,
     debug: DEBUG
 }));
-// Static/ Public Folder ("www")
 app.use(express.static(PUBLIC_DIR));
-// Body Parser
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
-// Form Parser
 app.use(formidable({
     uploadDir: "public/images",
     keepExtensions: true
 }));
-// Listen
 app.set("port", process.env.PORT || PORT);
 app.listen(PORT, function () {
     console.log("Server started on port " + PORT);
 });
-// Routes
 var data = {
     title: "StreamCollada",
     css: ["main.css"],
--- public/js/collada_stream.js
+++ public/js/collada_stream.js
@@ -1,7 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var THREE = require("three");
 var ColladaStream = (function () {
     function ColladaStream() {
+        this.loader = new THREE.ColladaLoader();
+        this.loader.load('models/collada/monster/monster.dae', function (collada) {
+            scene.add(collada.scene);
+        }, function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        });
     }
     return ColladaStream;
 }());
